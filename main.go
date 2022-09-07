de65835b13d0bddc69cbccb778aac5fb3c314c6b
PnKrT02 <pankfarewell@gmail.com>
2022-09-07 21:59:04 +0300

    done all tasks

--- main.go
+++ main.go
@@ -5,23 +5,24 @@ import ("fmt"
 "html/template")
 
 type User struct{
-  name string
-  age uint16 // целое неотризательное число
-  money int16
-  avg_grades, happiness float64
+  Name string
+  Age uint16 // целое неотризательное число
+  Money int16
+  Avg_grades, Happiness float64
+  Hobbies []string
 }
 
 func (u User) getAllInfo() string {
   return fmt.Sprintf("User name is: %s. He is %d and he has :%d dollars",
-    u.name, u.age, u.money)
+    u.Name, u.Age, u.Money)
 }
 
 func (u *User) setNewName(newName string) {
-  u.name = newName
+  u.Name = newName
 }
 
 func home_page(w http.ResponseWriter, r *http.Request) {
-jack := User{"Jack",27, -50, 4.3, 0.7}
+jack := User{"Jack",27, -50, 4.3, 0.7, []string{"Game", "Dance", "Night"}}
 /*jack.setNewName("Jessica")
   fmt.Fprintf(w, jack.getAllInfo())*/
   tmpl, _ := template.ParseFiles("templates/home_page.html")
@@ -39,7 +40,5 @@ func handleRequest() {
 }
 func main() {
   //jack := User{name: "Jack", age: 27, money: -50, avg_grades: 4.3, happiness: 0.7}
-
 handleRequest()
-
 }
